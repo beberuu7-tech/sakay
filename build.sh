76c83f3e6dc07f00ec651340c48fe34b01268401
unknown <lienujacnalballiv@gmail.com>
2025-11-25 12:27:42 +0800

    Fix build.sh permissions and line endings

--- build.sh
+++ build.sh
@@ -2,9 +2,14 @@
 # exit on error
 set -o errexit
 
+# Upgrade pip
 pip install --upgrade pip
+
+# Install dependencies
 pip install -r requirements.txt
 
+# Collect static files
 python manage.py collectstatic --no-input
-python manage.py migrate
 
+# Run migrations
+python manage.py migrate
