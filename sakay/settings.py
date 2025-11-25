b395eb4704d563ed6bbaa0af356ce1c6aa747894
unknown <lienujacnalballiv@gmail.com>
2025-11-25 20:17:32 +0800

    fix

--- sakay/settings.py
+++ sakay/settings.py
@@ -40,7 +40,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'myapp.middleware.ThreeTierAccessMiddleware',
+    #'myapp.middleware.ThreeTierAccessMiddleware',
 ]
 
 ROOT_URLCONF = 'sakay.urls'
